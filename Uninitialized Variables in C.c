f9dc9615791b92e9a1f5fcf36ee79de40fa9de81
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-11-01 23:22:29 +0600

    Implement C program with uninitialized variables
    Added a C program demonstrating uninitialized variables.

---
+++ Uninitialized Variables in C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+int a;
+float b;
+char c;
+double d;
+
+printf("int is %d\n",a);
+printf("float is %f\n",b);
+printf("char is %c\n",c);
+printf("double is %lf\n",d);
+
+return 0;
+}
+
