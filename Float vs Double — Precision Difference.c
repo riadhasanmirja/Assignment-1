b0237d3c742202e7296db8094455718fb355eae0
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-11-03 19:51:32 +0600

    Create Float vs Double — Precision Difference.c

---
+++ Float vs Double — Precision Difference.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float x = 7.081928373654f;
+    double y = 7.081928373654;
+    
+    printf("Float is : %.15f\n",x);
+    printf("Double is : %.15lf\n",y);
+    
+    return 0;
+}
