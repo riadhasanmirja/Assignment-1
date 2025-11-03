a5815eb85a74810bb40827895209b27dc871d000
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-11-03 20:02:59 +0600

    Create Swap Two Numbers Without Temp Variable.c

---
+++ Swap Two Numbers Without Temp Variable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 8;
+    int b = 5;
+    
+    printf ("Before Swap : a = %d , b = %d\n",a,b);
+    printf ("After Swap : a = %d , b = %d\n",b,a);
+    
+    return 0;
+}
