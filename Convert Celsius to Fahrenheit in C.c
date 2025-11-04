ee480df43bdc82bbe81770e7f36819502c0bd8f6
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-11-04 23:00:51 +0600

    Add Celsius to Fahrenheit conversion program
    Implement conversion from Celsius to Fahrenheit.

---
+++ Convert Celsius to Fahrenheit in C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    float C;
+    
+    printf("Celsius valu is :");
+    scanf ("%f",&C);
+    
+    float F = C*9/5+32;
+    printf ("Farenhite value is : %f",F);
+    
+    return 0;  
+}
