9d8ddd3b8fed561de8f13b5acef4e13fb2333601
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-11-04 22:33:37 +0600

    Create Print Float with 2 Decimal Places in C.c

---
+++ Print Float with 2 Decimal Places in C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float a;
+    
+    printf("Enter your float number: ");
+    scanf("%f", &a);
+    printf("Float 2 decimal number : %.2f\n", a);
+
+    return 0;
+}
